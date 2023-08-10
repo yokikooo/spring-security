123808d5dd48ea0898c4e6a591d52564724a17e9
yokikooo <ogi.stefanova@gmail.com>
2023-08-10 14:43:09 +0300

    disable CSRF so that spring is not creating it and set the session to be STATELESS

---
+++ learn-spring-security/src/main/java/com/diog/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.diog.basic;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (requests) -> {
+                    requests.anyRequest().authenticated();
+                });
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
